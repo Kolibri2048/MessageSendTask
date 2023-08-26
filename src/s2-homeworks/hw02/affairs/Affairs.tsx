725d424246dc226af86ca77dff4c295ea9f75453
Kolibri2048 <blak_fog@mail.ru>
2023-08-26 20:53:45 +0200

    homeWork-tuest: HW2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -2,33 +2,40 @@ import React from 'react'
 import Affair from './affair/Affair'
 import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
+import affair from "./affair/Affair";
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType> // need to fix any
+    setFilter: (filter:FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
-function Affairs(props: AffairsPropsType) {
+export function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter('all')
     }
     const setHigh = () => {
         // need to fix
+        props.setFilter('high')
+
+
     }
     const setMiddle = () => {
         // need to fix
+        props.setFilter('middle')
+
     }
     const setLow = () => {
         // need to fix
+        props.setFilter('low')
+
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
     const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
     const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
     const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
-
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
@@ -39,7 +46,9 @@ function Affairs(props: AffairsPropsType) {
 
     return (
         <div>
-            <div className={s.buttonContainer}>
+
+                <div className={s.buttonContainer}>
+
                 <button
                     id={'hw2-button-all'}
                     onClick={setAll}
@@ -69,7 +78,7 @@ function Affairs(props: AffairsPropsType) {
                     Low
                 </button>
             </div>
-            <div className={s.affairs}>{mappedAffairs}</div>
+            <div className={props.filter === 'all' ? s.affairs : s.affairsFilter}>{mappedAffairs}</div>
         </div>
     )
 }
